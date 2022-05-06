a09647a70331252032dcd43e34f9403490a2042a
elvisRD <animeamv687@gmail.com>
2022-05-06 01:58:30 -0400

    added ChakraProvider in app.tsx

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import {Button} from '@chakra-ui/react'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
