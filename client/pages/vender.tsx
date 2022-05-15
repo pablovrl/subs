7314e6b96ffcf43a57577a960cad395597199e1c
elvisRD <animeamv687@gmail.com>
2022-05-15 18:34:52 -0400

    Added sell screen

---
+++ client/pages/vender.tsx
@@ -0,0 +1,87 @@
+import React from "react";
+import { Box, Flex, Text, Image } from "@chakra-ui/react";
+import ButtonLink from "../components/ButtonLink";
+
+import type { NextPage } from "next";
+
+const Vender: NextPage = () => {
+	return (
+		<>
+			<Box backgroundColor={"black"}>
+				<Flex justifyContent={"center"}>
+					<Text
+						fontSize={"3xl"}
+						paddingTop={5}
+						textColor={"white"}
+						textAlign={"center"}
+						paddingX={5}
+						marginTop={1}
+					>
+						Miles de compradores están esperando para comprar tu caja
+					</Text>
+				</Flex>
+				<Flex justifyContent={"center"} marginTop={5}>
+					<Image
+						src="https://w0.peakpx.com/wallpaper/561/739/HD-wallpaper-box-machine-tec-abstract-black-blue-ios-lg-red-samsung-technology-vivid.jpg"
+						alt="Dan Abramov"
+					/>
+				</Flex>
+				<Flex justifyContent={"center"}>
+					<Text
+						fontSize={"2xl"}
+						textColor={"white"}
+						textAlign={"center"}
+						paddingX={5}
+					>
+						xxxxxxx es el primer y unico Marketplace de chile para emprendedores
+						de cajas de suscripción. Aprovecha ahora y concentrate en tu pasión,
+						nosotros haremos el resto. !ES TOTALMENTE GRATIS¡
+					</Text>
+				</Flex>
+				<Flex justifyContent={"center"} marginTop={10} paddingBottom={10}>
+					<ButtonLink text="Crear cuenta" href="/Registrar" fontSize={"2xl"} />
+				</Flex>
+			</Box>
+			<Box backgroundColor={"white"}>
+				<Flex justifyContent={"center"}>
+					<Text
+						fontSize={"3xl"}
+						paddingTop={5}
+						textAlign={"center"}
+						paddingX={5}
+						marginTop={1}
+					>
+						Vende tus cajas aquí y aprovecha las ventajas de la aplicacion.
+					</Text>
+				</Flex>
+				<Flex justifyContent={"center"} marginTop={5}>
+					<Image
+						src="https://areajugones.sport.es/wp-content/uploads/2017/10/UtraInstintoDragonBall.jpg.webp"
+						alt="Dan Abramov"
+					/>
+				</Flex>
+				<Flex justifyContent={"center"}>
+					<Text
+						fontSize={"3xl"}
+						paddingTop={5}
+						textAlign={"center"}
+						paddingX={5}
+						marginTop={1}
+					>
+						¡Unete a la aplicacion junto con varios comerciantes que hacen
+						crecer sus negocios gracias a xxxxxxxxx.
+					</Text>
+				</Flex>
+				<Flex justifyContent={"center"} marginTop={5} paddingBottom={5}>
+					<ButtonLink
+						text="Añadir Producto"
+						href="/agregarProducto"
+						fontSize={"2xl"}
+					/>
+				</Flex>
+			</Box>
+		</>
+	);
+};
+
+export default Vender;
