56bc42ed6a07b0cadb6a6365f91b5d3110ad9c27
Pablo Villarroel <pablovillarroel135@gmail.com>
2022-05-15 15:32:12 -0400

    create SearchInput component

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,23 +1,13 @@
 import React from "react";
-import { Box, Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import type { NextPage } from "next";
-import CustomerFavorites from "../components/CustomerFavorites";
 import Navbar from "../components/Navbar";
-import { FaSistrix } from "react-icons/fa";
 
 const Home: NextPage = () => {
 	return (
 		<Box>
 			<Navbar />
-			<Box mx={4}>
-				<InputGroup>
-					<InputLeftElement>
-						<FaSistrix size={20} />
-					</InputLeftElement>
-					<Input placeholder="Buscar ..." variant={"filled"} zIndex={1} />
-				</InputGroup>
-				<CustomerFavorites />
-			</Box>
+			<Box mx={4}></Box>
 		</Box>
 	);
 };
