42fcc20ee474d5effb38ea164925a6a88c897cc7
Pablo Villarroel <pablovillarroel135@gmail.com>
2022-06-14 16:18:57 -0400

    add suscription models

---
+++ server/src/models/suscriptor.ts
@@ -0,0 +1,24 @@
+import { db } from "../config/database";
+import { DataTypes } from "sequelize";
+
+const Suscriptor = db.define(
+  "suscriptor",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    nombreCompleto: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    rut: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  { timestamps: false }
+);
+
+export default Suscriptor;
