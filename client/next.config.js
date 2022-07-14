f040bb78a1ebbfecc0336c0eda91652b5ab45577
Pablo Villarroel <pablovillarroel135@gmail.com>
2022-07-14 00:21:07 -0400

    add enviroment values

--- client/next.config.js
+++ client/next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
 	images: {
 		domains: ["localhost"],
 	},
+	env: {
+		URL: process.env.URL,
+		DB: process.env.DB,
+		DB_USER: process.env.DB_USER,
+		DB_PASS: process.env.DB_PASS,
+		DB_HOST: process.env.DB_HOST,
+		DB_DIALECT: process.env.DB_DIALECT,
+	},
 };
 
 module.exports = nextConfig;
