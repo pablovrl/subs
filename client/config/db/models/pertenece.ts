eb5bae93950c4deeb07e3aee112e535009f404a9
Pablo Villarroel <pablovillarroel135@gmail.com>
2022-07-14 00:21:50 -0400

    add backend config and models

---
+++ client/config/db/models/pertenece.ts
@@ -0,0 +1,5 @@
+import { db } from "../database";
+const Pertenece = db.define("pertenece", {}, { timestamps: false });
+
+
+export default Pertenece;
---
+++ client/config/multer.ts
@@ -0,0 +1,22 @@
+import multer from "multer";
+import { v4 as uuidv4 } from "uuid";
+import fs from "fs";
+import path from "path";
+
+const dir = "./public/uploads";
+if (!fs.existsSync(dir)) {
+	fs.mkdirSync(dir, { recursive: true });
+}
+
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, "./public/uploads");
+	},
+	filename: function (req, file, cb) {
+		cb(null, uuidv4() + path.extname(file.originalname));
+	},
+});
+
+const saveToUploads = multer({ storage: storage });
+
+export default saveToUploads;
