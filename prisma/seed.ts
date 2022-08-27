3da3bd7c7953b1a53c732b4a37966b146588ab2c
Pablo Villarroel <pablovillarroel135@gmail.com>
2022-08-26 20:51:45 -0400

    create prisma schema and seeds

---
+++ prisma/seed.ts
@@ -0,0 +1,67 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+async function main() {
+	const credencials = await prisma.credencial.createMany({
+		data: [
+			{
+				email: "pablovillarroel135@gmail.com",
+				password: "colocolo123",
+				tipoUsuario: "vendedor",
+			},
+			{
+				email: "elvis@gmail.com",
+				password: "colocolo123",
+				tipoUsuario: "cliente",
+			},
+		],
+	});
+
+	const seller = await prisma.vendedor.create({
+		data: {
+			nombreCompleto: "Pablo Villarroel",
+			nombreTienda: "Pablo Shop",
+			imagenTienda: "",
+			credencialId: 1,
+		},
+	});
+
+	const suscriptor = await prisma.suscriptor.create({
+		data: {
+			nombreCompleto: "Elvis Rodríguez",
+			rut: "20255000-1",
+			credencialId: 2,
+		},
+	});
+
+	const categories = [
+		"Mascotas",
+		"Entretenimiento",
+		"Videojuegos",
+		"Juegos de Mesa",
+		"Consumibles",
+		"Libros",
+		"Regalos",
+		"Baño y Belleza",
+		"Deporte",
+		"Adulto",
+	];
+
+	const newCategories = await prisma.categoria.createMany({
+		data: categories.map((el) => ({
+			nombre: el,
+		})),
+	});
+
+	console.log(credencials, seller, suscriptor, newCategories);
+}
+
+main()
+	.then(async () => {
+		await prisma.$disconnect();
+	})
+	.catch(async (e) => {
+		console.error(e);
+		await prisma.$disconnect();
+		process.exit(1);
+	});
