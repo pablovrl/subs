2dff46651e1f98d497b100a5c01f1f1d005d843a
Pablo Villarroel <pablovillarroel135@gmail.com>
2022-09-01 14:41:20 -0400

    create endpoint to show images

---
+++ pages/api/image/[...slug].ts
@@ -0,0 +1,12 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import fs from "fs";
+import path from "path";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+	const imageName = req.query.slug as string[];
+	const imagePath = imageName.join("/");
+	const filePath = path.resolve(".", `public/${imagePath}`);
+	const imageBuffer = fs.readFileSync(filePath);
+	res.setHeader("Content-Type", "image/jpg");
+	return res.send(imageBuffer);
+}
