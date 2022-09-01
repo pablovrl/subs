262de45487f75757a54eb04d7bf607d175a8c332
Pablo Villarroel <pablovillarroel135@gmail.com>
2022-09-01 12:42:43 -0400

    display message when there are no reviews

--- components/Reviews/index.tsx
+++ components/Reviews/index.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import { Box, Heading, Text } from "@chakra-ui/react";
+import { Alert, AlertIcon, Box, Heading, Text } from "@chakra-ui/react";
 import { Valoracion } from "../../interfaces/Valoraciones";
 import StarsRating from "react-star-rate";
-import ReviewModal from "../ReviewModal";
 
 const Reviews = ({ valoraciones }: { valoraciones: Valoracion[] }) => {
 	return (
@@ -10,19 +9,29 @@ const Reviews = ({ valoraciones }: { valoraciones: Valoracion[] }) => {
 			<Heading size="lg" my="4">
 				Opiniones sobre el producto
 			</Heading>
-			{valoraciones.map((val) => (
-				<Box key={val.id} mb={4} width="40rem">
-					<StarsRating
-						style={{
-							style: { fontSize: "20px" },
-						}}
-						value={parseInt(val.estrellas, 10)}
-						disabled={true}
-					/>
-					<Text fontWeight="bold">{val.titulo}</Text>
-					<Text>{val.comentario}</Text>
-				</Box>
-			))}
+			{valoraciones.length === 0 ? (
+				<Alert status="info">
+					<AlertIcon />
+					Aún no hay opiniones sobre el producto, suscríbete y deja la tuya para
+					ayudar a otros compradores.{" "}
+				</Alert>
+			) : (
+				<>
+					{valoraciones.map((val) => (
+						<Box key={val.id} mb={4} width="40rem">
+							<StarsRating
+								style={{
+									style: { fontSize: "20px" },
+								}}
+								value={parseInt(val.estrellas, 10)}
+								disabled={true}
+							/>
+							<Text fontWeight="bold">{val.titulo}</Text>
+							<Text>{val.comentario}</Text>
+						</Box>
+					))}
+				</>
+			)}
 		</Box>
 	);
 };
