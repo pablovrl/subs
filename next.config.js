56421fbdef2fd9ba963ea300bdf57ef48032be59
Pablo Villarroel <pablovillarroel135@gmail.com>
2022-09-05 10:10:56 -0400

    remove unused env variables

--- next.config.js
+++ next.config.js
@@ -6,11 +6,6 @@ const nextConfig = {
 	},
 	env: {
 		URL: process.env.URL,
-		DB: process.env.DB,
-		DB_USER: process.env.DB_USER,
-		DB_PASS: process.env.DB_PASS,
-		DB_HOST: process.env.DB_HOST,
-		DB_DIALECT: process.env.DB_DIALECT,
 	},
 };
 
